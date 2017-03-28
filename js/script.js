b61b46094fc8292e7036d52f22a0164fa6247e19
caophuc0912 <caophuc0912@gmail.com>
2017-03-28 17:21:21 +0700

    add

---
+++ js/script.js
@@ -0,0 +1,21 @@
+
+
+	
+
+	// 	myApp.onPageInit('login-screen', function (page) {
+	// 	  var pageContainer = $$(page.container);
+	// 	  pageContainer.find('.list-button').on('click', function () {
+	// 	    var username = pageContainer.find('input[name="username"]').val();
+	// 	    var password = pageContainer.find('input[name="password"]').val();
+	// 	    // Handle username and password
+	// 	    myApp.alert('Username: ' + username + ', Password: ' + password, function () {
+	// 	      mainView.goBack();
+	// 	    });
+	//   });
+	// });
+
+
+	
+
+
+ 
